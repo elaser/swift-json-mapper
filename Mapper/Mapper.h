faa5c5eb2338a519f28725ed3ac7f6943b0c6927
Anderthan Hsieh <anderthan@gmail.com>
2016-10-01 18:24:57 -0700

    Initial Commit

---
+++ Mapper/Mapper.h
@@ -0,0 +1,19 @@
+//
+//  Mapper.h
+//  Mapper
+//
+//  Created by Anderthan Hsieh on 10/1/16.
+//  Copyright © 2016 anderthan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Mapper.
+FOUNDATION_EXPORT double MapperVersionNumber;
+
+//! Project version string for Mapper.
+FOUNDATION_EXPORT const unsigned char MapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Mapper/PublicHeader.h>
+
+
